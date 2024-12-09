2215280e5b302b30c117f170c5a7ce6e80ab61c5
nathidaum <daumnathi@gmail.com>
2024-12-09 13:56:05 +0100

    Initial setup and cleanup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// App.tsx
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>Hi</h1>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
