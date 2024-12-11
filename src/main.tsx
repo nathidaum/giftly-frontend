9a5461aa34b4107103b82712aba5836e066dccb9
nathidaum <daumnathi@gmail.com>
2024-12-11 08:09:56 +0100

    feat: implement mantine component library

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <MantineProvider>
+        <App />
+      </MantineProvider>
     </BrowserRouter>
   </StrictMode>
 );
